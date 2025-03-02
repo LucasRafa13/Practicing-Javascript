ad13f0b0929bb57234626630ce2a721df3b89e87
Lucas Lima <lucasrafael123araujolima@gmail.com>
2025-03-02 12:30:44 -0400

    Project Refund

--- Refund/scripts.js
+++ Refund/scripts.js
@@ -6,6 +6,8 @@ const category = document.getElementById('category')
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector('ul')
+const expensesTotal = document.querySelector('aside header h2')
+const expensesQuantity = document.querySelector('aside header p span')
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, '')
@@ -64,13 +66,91 @@ function expenseAdd(newExpense) {
     const expenseCategory = document.createElement('span')
     expenseCategory.textContent = newExpense.category_name
 
+    // Adiciona nome e categoria na div das informalções da despesa
+    expenseInfo.append(expenseName, expenseCategory)
+
+    // Criar o valor da despesa
+
+    const expenseAmount = document.createElement('span')
+    expenseAmount.classList.add('expense-amount')
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace('R$', '')}`
+
+    // Cria o remover
+    const removeIcon = document.createElement('img')
+    removeIcon.classList.add('remove-icon')
+    removeIcon.setAttribute('src', 'img/remove.svg')
+    removeIcon.setAttribute('alt', 'remover')
+
     // Adiciona as informações no item
-    expenseItem.append(expenseIcon)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
     // Adiciona o item na lista
     expenseList.append(expenseItem)
+
+    formClear()
+
+    //Atualiza os totais
+    updateTotals()
   } catch (error) {
     console.log(error)
     alert('Ocorreu um erro ao adicionar o gasto. Tente novamente.')
   }
+}
+
+function updateTotals() {
+  try {
+    const items = expenseList.children
+
+    expensesQuantity.textContent = `${items.length} ${
+      items.length > 1 ? 'despesas' : 'despesa'
+    }`
+
+    let total = 0
+
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector('.expense-amount')
+
+      let value = itemAmount.textContent
+        .replace(/[^\d,]/g, '')
+        .replace(',', '.')
+
+      value = parseFloat(value)
+
+      if (isNaN(value)) {
+        return alert('Não foi possível calcular o total.')
+      }
+
+      total += Number(value)
+    }
+
+    const symbolBRL = document.createElement('small')
+    symbolBRL.textContent = 'R$'
+
+    total = formatCurrentBRL(total).toUpperCase().replace('R$', '')
+
+    expensesTotal.innerHTML = ''
+
+    expensesTotal.append(symbolBRL, total)
+  } catch (error) {
+    console.log(error)
+    alert('Não foi possível atualizar os totais')
+  }
+}
+
+expenseList.addEventListener('click', function (event) {
+  if (event.target.classList.contains('remove-icon')) {
+    const item = event.target.closest('.expense')
+    item.remove()
+  }
+  updateTotals()
+})
+
+function formClear() {
+  expense.value = ''
+  category.value = ''
+  amount.value = ''
+
+  expense.focus()
 }
